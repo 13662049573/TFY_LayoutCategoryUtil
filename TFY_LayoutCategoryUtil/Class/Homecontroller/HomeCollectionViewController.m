a0d668ffb8d40d6c3d381cfadbcb225b621068b8
420144542@qq.com <420144542@qq.com>
2020-08-18 15:39:13 +0800

    有啊

--- TFY_LayoutCategoryUtil/Class/Homecontroller/HomeCollectionViewController.m
+++ TFY_LayoutCategoryUtil/Class/Homecontroller/HomeCollectionViewController.m
@@ -9,7 +9,7 @@
 #import "HomeCollectionViewController.h"
 
 @interface HomeCollectionViewController ()
-
+@property (nonatomic, strong) UIButton * confirmButton;
 @end
 
 @implementation HomeCollectionViewController
@@ -17,8 +17,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor blueColor];
+    
+    UIButtonModelSet().backgroundColor(UIColor.orangeColor).text(@"按钮", UIControlStateNormal).font([UIFont systemFontOfSize:15 weight:UIFontWeightBold]).assignTo(^(__kindof UIView * _Nonnull view) {
+        self.confirmButton = view;
+        
+    }).addTarget(self, @selector(btnClick), UIControlEventValueChanged).addToSuperView(self.view).makeMasonry(^(MASConstraintMaker * _Nonnull make) {
+        make.left.equalTo(self.view).offset(20);
+        make.top.equalTo(self.view).offset(100);
+        make.right.equalTo(self.view).offset(-20);
+        make.height.mas_equalTo(50);
+    });
 }
 
-
+- (void)btnClick {
+    
+}
 
 @end
