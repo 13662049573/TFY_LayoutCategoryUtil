075ace7f73c9d41f32fa94096b7fb9ed671e23e9
420144542@qq.com <420144542@qq.com>
2020-08-19 17:54:01 +0800

    Update TFY_LayoutCategoryHeader.h

--- TFY_LayoutCategoryUtil/TFY_LayoutCategoryKit/TFY_LayoutCategoryHeader.h
+++ TFY_LayoutCategoryUtil/TFY_LayoutCategoryKit/TFY_LayoutCategoryHeader.h
@@ -4,7 +4,7 @@
 //
 //  Created by tiandengyou on 2020/3/30.
 //  Copyright © 2020 田风有. All rights reserved.
-//  最新版本号：1.1.6
+//  最新版本号：1.2.0
 
 #ifndef TFY_LayoutCategoryHeader_h
 #define TFY_LayoutCategoryHeader_h
@@ -14,5 +14,6 @@
 #import "TFY_ChainHeader.h"
 /**额外工具*/
 #import "TFY_ToolsHeader.h"
+
 
 #endif /* TFY_LayoutCategoryHeader_h */
