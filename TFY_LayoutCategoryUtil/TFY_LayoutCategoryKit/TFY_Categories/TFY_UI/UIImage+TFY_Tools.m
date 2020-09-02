e42786c1c3dbf483633a7bf4b2c01cc1737edce6
420144542@qq.com <420144542@qq.com>
2020-09-02 09:14:29 +0800

    优化

--- TFY_LayoutCategoryUtil/TFY_LayoutCategoryKit/TFY_Categories/TFY_UI/UIImage+TFY_Tools.m
+++ TFY_LayoutCategoryUtil/TFY_LayoutCategoryKit/TFY_Categories/TFY_UI/UIImage+TFY_Tools.m
@@ -410,17 +410,17 @@ static NSTimeInterval _CGImageSourceGetGIFFrameDelayAtIndex(CGImageSourceRef sou
             rect.size.height = asize.height;
             rect.origin.x = (asize.width - rect.size.width)/2;
             rect.origin.y = 0;
-        } else {
+         } else {
             rect.size.width = asize.width;
             rect.size.height = asize.width*oldsize.height/oldsize.width;
             rect.origin.x = 0;
             rect.origin.y = (asize.height - rect.size.height)/2;
-      }
-      UIGraphicsBeginImageContext(asize);
-      CGContextRef context = UIGraphicsGetCurrentContext();
-      CGContextSetFillColorWithColor(context, [[UIColor clearColor] CGColor]);
-      UIRectFill(CGRectMake(0, 0, asize.width, asize.height));//clear background
-      [image drawInRect:rect];
+        }
+        UIGraphicsBeginImageContext(asize);
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        CGContextSetFillColorWithColor(context, [[UIColor clearColor] CGColor]);
+        UIRectFill(CGRectMake(0, 0, asize.width, asize.height));//clear background
+        [image drawInRect:rect];
         newimage = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext();
     }
