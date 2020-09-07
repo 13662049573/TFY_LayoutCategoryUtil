4a8a84b2091d0453560b57a359a2b69df909ad11
420144542@qq.com <420144542@qq.com>
2020-09-07 14:21:04 +0800

    youhua

--- TFY_LayoutCategoryUtil/TFY_LayoutCategoryKit/TFY_LayoutCategoryHeader.h
+++ TFY_LayoutCategoryUtil/TFY_LayoutCategoryKit/TFY_LayoutCategoryHeader.h
@@ -5,7 +5,11 @@
 //  Created by tiandengyou on 2020/3/30.
 //  Copyright © 2020 田风有. All rights reserved.
 //  最新版本号：1.5.8
-
+/**
+   使用说明：
+   只要是类别的方法，所有方法前面，都会加 tfy_   如：[self.window tfy_showOnScene:scene];
+   如果是单列，或者一些工具方法，前面没有加任何修饰。  如：[TFY_Utils judgeIsEmptyWithString:@""];
+ */
 
 #ifndef TFY_LayoutCategoryHeader_h
 #define TFY_LayoutCategoryHeader_h
