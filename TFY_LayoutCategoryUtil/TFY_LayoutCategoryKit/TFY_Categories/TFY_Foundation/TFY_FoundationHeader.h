168bd6e9bdf40d6699c731461fdfdc1ddef9f591
田风有 <420144542@qq.com>
2020-12-13 19:32:06 +0800

    youhua

--- TFY_LayoutCategoryUtil/TFY_LayoutCategoryKit/TFY_Categories/TFY_Foundation/TFY_FoundationHeader.h
+++ TFY_LayoutCategoryUtil/TFY_LayoutCategoryKit/TFY_Categories/TFY_Foundation/TFY_FoundationHeader.h
@@ -27,6 +27,6 @@
 #import "UIAlertController+TFY_Chain.h"
 #import "NSMutableAttributedString+TFY_Tools.h"
 #import "NSMutableParagraphStyle+TFY_Chain.h"
-
+#import "UIBezierPath+TFY_Tools.h"
 
 #endif /* TFY_FoundationHeader_h */
