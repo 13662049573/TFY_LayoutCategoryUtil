a369592f2371283535154a0459d250f255d9efd2
田风有 <420144542@qq.com>
2021-03-13 11:38:44 +0800

    修改问题

---
+++ TFY_LayoutCategoryUtil/Class/Homecontroller/EmptyTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  EmptyTableViewController.h
+//  TFY_LayoutCategoryUtil
+//
+//  Created by 田风有 on 2021/3/13.
+//  Copyright © 2021 田风有. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EmptyTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
